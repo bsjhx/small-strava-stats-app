9cdbbd981701d64da23faddcf04e767742c266f2
Michal Rakoczy <rakoczymichal@hotmail.com>
2022-07-27 07:02:34 +0200

    Added authorization with strava

---
+++ client/src/routes/exchange_token.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+	const code = $page.url.searchParams.get('code');
+</script>
+
+<svelte:head>
+	<title>Lol</title>
+	<meta name="description" content="Strava authorized" />
+</svelte:head>
+
+<div class="content">
+	{#if code}
+		<h1>You did it! Strava is authorized. Now just let us sync all your activities:</h1>
+		Code: {code}
+		<button>Start sync</button>
+	{:else}
+		<h1>App is not authorized... Sorry about that</h1>
+	{/if}
+</div>
+
+<style>
+	.content {
+		width: 100%;
+		max-width: var(--column-width);
+		margin: var(--column-margin-top) auto 0 auto;
+	}
+</style>
--- client/src/routes/index.svelte
+++ client/src/routes/index.svelte
@@ -4,6 +4,7 @@
 
 <script lang="ts">
 	import Counter from '$lib/Counter.svelte';
+	import Linkable from '$lib/Linkable.svelte';
 </script>
 
 <svelte:head>
@@ -12,21 +13,9 @@
 </svelte:head>
 
 <section>
-	<h1>
-		<span class="welcome">
-			<picture>
-				<source srcset="svelte-welcome.webp" type="image/webp" />
-				<img src="svelte-welcome.png" alt="Welcome" />
-			</picture>
-		</span>
-
-		to your new<br />SvelteKit app :)
-	</h1>
-
-	<h2>
-		try editing <strong>src/routes/index.svelte</strong>
-	</h2>
-
+	<Linkable link = 'http://www.strava.com/oauth/authorize?client_id=72239&response_type=code&redirect_uri=http://localhost/exchange_token&approval_prompt=force&scope=activity:read_all'>
+		Authorize strava!
+	</Linkable>
 	<Counter />
 </section>
 
